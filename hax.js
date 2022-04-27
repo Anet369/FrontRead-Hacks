d33eba52e99cb9e7e43047220fafdc730a39276c
Anet369 <46793493+Anet369@users.noreply.github.com>
2022-04-27 19:59:02 +0000

    Update hax.js

--- hax.js
+++ hax.js
@@ -1,41 +1,94 @@
-function GetInfoByUniLogin(uniLogin, callback){
-    Authenticator.authenticateUni({unicID: uniLogin}, (random, data) => {
-        callback(data);
-    })
-}
-function LoginAs(uniLogin) {
-    GetInfoByUniLogin(uniLogin, data => Server.loadUserData());
-}
-$("body").on("keydown", e => {
-    switch (e.key) {
-        case " ":
-            if (App.currentModule.includes("m") && Number.isInteger(App.currentLevel) && App.currentExercise != undefined && Number.isInteger(App.currentAttempt)) {
-        
-                moduleIndex = Number.parseInt(App.currentModule.split("m")[1]) - 1;
-                levelIndex = (App.currentLevel - 1) % 3;
-                exerciseIndex = Number.parseInt(App.currentExercise.split("f")[1]) - 1;
-                attemptIndex = App.currentAttempt - 1;
-        
-                let attempt = App.userdata.modules[moduleIndex].levels[levelIndex].exercises[exerciseIndex].attempts[attemptIndex];
-        
-                let correct = prompt("How many correct (tip: you can enter letters as well)", 10);
-                let wrong = prompt("How many wrong", 0);
-                let speed = prompt("Blink time", 0.04);
-                let sure = confirm(`Your score will be ${correct}/${wrong}: ${speed}`);
-        
-                if (sure) {
-                    attempt.correctCount = correct;
-                    attempt.wrongCount = wrong;
-                    attempt.blinkSpeed = speed;
-                    Server.saveUserData();
-                }
+Hax = {
+    // InComplete
+    inCompleteAllExercises: function() {
+        let module = 0;
+        for (let level = 1; level <= 9; level++) {
+            if ((level - 1) % 3 == 0) {
+                module++;
             }
-            return;
-        case "Enter":
-            let diplom = document.getElementsByClassName("diplom-box locked")[0];
-            if (diplom != undefined) {
-                diplom.classList.remove("locked");
+
+            for (let exercise = 1; exercise <= 4; exercise++) {
+                App.execute("entities:exercise:setInComplete", `m${module}`, `${level}`, `f${exercise}`);
             }
-            return;
+        }
+    },
+    inCompleteAllTests: function() {
+        for (let test of App.userdata.tests) {
+            test.isComplete = false;
+            test.correctCount = 0;
+            test.wrongCount = test.objectivesCount;
+        }
+    },
+    lockAllVideos: function() {
+        for (let video of App.userdata.videos) {
+            video.haveBeenWatched = false;
+            video.isUnlocked = false;
+        }
+    },
+    // Complete
+    completeAllExercises: function() {
+        let module = 0;
+        for (let level = 1; level <= 9; level++) {
+            if ((level - 1) % 3 == 0) {
+                module++;
+            }
+
+            for (let exercise = 1; exercise <= 4; exercise++) {
+                App.execute("entities:exercise:setComplete", `m${module}`, `${level}`, `f${exercise}`);
+            }
+        }
+    },
+    completeAllTests: function() {
+        for (let test of App.userdata.tests) {
+            test.isComplete = true;
+            test.correctCount = test.objectivesCount;
+            test.wrongCount = 0;
+        }
+    },
+    unlockAllVideos: function() {
+        for (let video of App.userdata.videos) {
+            video.haveBeenWatched = true;
+            video.isUnlocked = true;
+        }
+    },
+    unlockDiploma: function() {
+        this.completeAllExercises();
+        this.unlockAllVideos();
+        this.completeAllTests();
+
+        Server.saveUserData();
+    },
+    // Reset
+    resetAllExercises: function() {
+        for (module of App.userdata.modules) {
+            for (level of module.levels) {
+                for (exercise of level.exercises) {
+                    for (attempt of exercise.attempts) {
+                        attempt.wrongCount = 0;
+                        attempt.correctCount = 0;
+                        attempt.isComplete = false;
+                        attempt.blinkSpeed = null;
+                    }
+                }	
+            }
+        }
+    },
+    resetProgress: function() {
+        if (confirm("Are you sure, this will delete all progress") === false) 
+        return;
+        
+        this.inCompleteAllExercises();
+        this.resetAllExercises();
+        this.lockAllVideos();
+        this.inCompleteAllTests();
+
+        Server.saveUserData();
+    },
+    // Impersonate
+    impersonate: function(uniLogin){
+        Authenticator.authenticateUni({unicID: uniLogin}, () => {
+            Server.loadUserData();
+            alert("You are now logged in as: " + uniLogin);
+        });
     }
-});
+}
